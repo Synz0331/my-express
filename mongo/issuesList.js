89a642205f98233d33d08750ded97490b2d9d9fd
Synz0331 <eeakins06@gmail.com>
2015-02-15 13:21:08 -0700

    Completed all the files for viewing in mongo.  Have not pushed to Heroku

---
+++ mongo/issuesList.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+    
+    app.get('/list/issues', function(req, res) {
+
+        var Issues = app.db.get('issues')
+
+        Issues.find({}, function(err, iss) {
+
+            res.render('listIssues.jade', {
+                Issues: iss
+            })
+        })
+
+    })
+}
---
+++ mongo/pullsList.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+    
+    app.get('/list/pulls', function(req, res) {
+
+        var pulls = app.db.get('pulls')
+
+        pulls.find({}, function(err, pull) {
+
+            res.render('listPulls.jade', {
+                pulls: pull
+            })
+        })
+
+    })
+}
